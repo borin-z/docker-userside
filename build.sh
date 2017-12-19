723b7764261bb67c21e3f7e22bed71f7888503fc
root <root@userside.navi.com>
2017-12-19 16:44:58 +0500

    Исправил context для build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t borin/userside:php5.6-fpm -f fpm-5.6/Dockerfile . && \
+docker build -t borin/userside:php5.6-fpm -f fpm-5.6/Dockerfile ./fpm-5.6 && \
 docker push borin/userside:php5.6-fpm && \
-docker build -t borin/userside:postgres-10 -f postgres-10/Dockerfile . && \
+docker build -t borin/userside:postgres-10 -f postgres-10/Dockerfile ./postgres-10 && \
 docker push borin/userside:postgres-10 && \
-docker build -t borin/userside:nginx-latest -f nginx/Dockerfile . && \
+docker build -t borin/userside:nginx-latest -f nginx/Dockerfile ./nginx && \
 docker push borin/userside:nginx-latest && \
 echo "build OK";
 
